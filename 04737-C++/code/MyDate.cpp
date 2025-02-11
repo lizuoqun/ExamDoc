f58d12eda59d70cdc01a0a709b6cd9c1e41f29c5
李佐群 <2645929971@qq.com>
2025-02-11 14:05:47 +0800

    feat(MyDate): 添加复制构造函数、静态函数和常量函数- 实现了 MyDate 类的复制构造函数 - 添加了静态变量 count 和相应的访问函数 getCount- 实现了一个常量函数 getCount2 - 新增了常量对象的定义和使用 -定义了 MyDate 类的析构函数

--- 04737-C++/code/MyDate.cpp
+++ 04737-C++/code/MyDate.cpp
@@ -7,6 +7,12 @@ public:
     // 构造函数
     MyDate();
     MyDate(int, int, int);
+    // 复制构造函数
+    MyDate(const MyDate &);
+    // 静态函数
+    static int getCount();
+    // 常量函数
+    int getCount2() const;
     void setDate(MyDate);
     MyDate getDate();
     void setYear(int);
@@ -15,7 +21,22 @@ public:
 private:
     // 成员变量
     int year, month, day;
+    static int count;
 };
+
+// 初始化静态变量
+int MyDate::count = 0;
+
+int MyDate::getCount()
+{
+    // 这里面不能访问非静态变量
+    return count;
+}
+
+int MyDate::getCount2() const
+{
+    cout << count << endl;
+}
 
 // 初始化类，有参、无参构造
 MyDate::MyDate()
@@ -25,11 +46,29 @@ MyDate::MyDate()
     day = 1;
 }
 
+// 无参构造，和上面相等
+// MyDate::MyDate():year(1970),month(1),day(1){}
+
 MyDate::MyDate(int y, int m, int d)
 {
     year = y;
     month = m;
     day = d;
+}
+
+// 有参构造改写
+// MyDate::MyDate(int y, int m, int d):year(y),month(m),day(d){};
+
+// 实现复制构造函数
+MyDate::MyDate(const MyDate &d)
+{
+    cout << "复制构造函数" << d.year << endl;
+}
+
+// 定义析构函数
+MyDate::~MyDate()
+{
+    cout << "MyDate析构函数" << endl;
 }
 
 void MyDate::setDate(MyDate d)
@@ -49,6 +88,10 @@ int main()
     MyDate md;
 
     MyDate *md1 = new MyDate(2001, 1, 2);
+
+    // 定义常量对象
+    const MyDate md2(2002, 2, 3);
+    MyDate const md3(2003, 3, 4);
 
     // 对象和对象指针调用方式
     cout << md.getYear() << endl;
